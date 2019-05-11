01c6df1da0f7545792bc0f9aa05b6386160c19ad
Tan Bui <tan@vaadin.com>
2019-05-11 12:16:35 +0300

    show failed login message

--- src/mason-app/mason-app.js
+++ src/mason-app/mason-app.js
@@ -28,7 +28,8 @@ class MasonApp extends PolymerElement {
 
   constructor() {
     super();
-    window.addEventListener("login-successful", (e) => this._afterLogin(e));
+    window.addEventListener("login-successful", (e) => this._loginSuccessful(e));
+    window.addEventListener("login-failed", (e) => this._loginFailed(e));
     window.masonAsyncInit = function () {
       Mason({
         apiKey: 'GO5UhSLGg73Sx9PAMGNfiXHN2GNK5hj9NGydqmRlggY=',
@@ -37,22 +38,25 @@ class MasonApp extends PolymerElement {
 
       Mason.callback('willSendData', function (form, name, componentId) {
         firebase.auth().signInWithEmailAndPassword(form.data.email, form.data.password).then(function (response) {
-          window.dispatchEvent(new CustomEvent('login-successful', {detail: form.data.email}));
+          window.dispatchEvent(new CustomEvent('login-successful', { detail: form.data.email }));
         })
-          .catch(function (error) {
-            console.log(error);
-          });
+        .catch(function (error) {
+          window.dispatchEvent(new CustomEvent('login-failed', { detail: form.data.email }));
+        });
         return true; // skip validation
       });
     };
     firebase.initializeApp(firebaseConfig);
   }
 
-  _afterLogin(e) {
+  _loginSuccessful(e) {
     this.$.login.remove();
     this.$.notification.innerHTML = `<p>Hi, ${e.detail}!<p>Welcome to FEaaS.<p></p><br/><br/>Checkout in <a target="_blank" href="https://github.com/tanbt/frontend-service-playaround">source code</a>.`;
   }
 
+  _loginFailed(e) {
+    this.$.notification.innerHTML = `Please try again.`;
+  }
 }
 
 window.customElements.define('mason-app', MasonApp);
